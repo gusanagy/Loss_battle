4469a5f05a0905cf00c20d4aa0f2dae28f52c907
Gusanagy <gustavo.pereira2146@gmail.com>
2024-07-31 14:12:11 -0300

    adicionadas mudancas

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+## carregar modelos
+## carregar funcoes de perda
+## carrregar datasets
+## Fazer combinacao de modelos e funcoes de perda
+## implementar ddp e torchrun para treinamento distribuido e acelerado
+## Treinar modelo 
+## avaliar com as metricas
