a851d50b0c90607b02b0242c879408be4d8c4c73
Gustavo Pereira de Almeida <65310167+gusanagy@users.noreply.github.com>
2024-08-01 14:17:59 -0300

    loss initial import adjusts (#2)

--- loss/__init__.py
+++ loss/__init__.py
@@ -1,4 +1,4 @@
-from kornia.losses import ssim_loss as ssim, psnr_loss as psnr, MS_SSIMLoss as ms_ssim, charbonnier_loss as charbonnier
-import lpips
-import torchvision.models as models
-from torch.nn import MSELoss #L2LOSS
+from .ChannelLoss import *
+from .perceptualLoss import *
+from .structuralLoss import *
+
