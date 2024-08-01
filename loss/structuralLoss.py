6950e74a9a6d12e76a801271ddcf6c7d0d5153a8
Gusanagy <gustavo.pereira2146@gmail.com>
2024-08-01 15:33:15 -0300

    aplicando index e ids para as funcoes para sua listagem e refatorando as funcoes perceptuais

--- loss/structuralLoss.py
+++ loss/structuralLoss.py
@@ -49,7 +49,64 @@ class L1Loss(nn.Module):
         loss = torch.mean(abs_diff)
         return loss
 
+"""Gradient Loss OpenCV"""
+class GradientLossOpenCV(nn.Module):
+    def __init__(self,id: int = None):
+        super(GradientLossOpenCV, self).__init__()
+        self._id = id
+    @property
+    def id(self):
+        return self._id
+    def name(self):
+        return self.__class__.__name__
+    
+    def compute_gradient(self, image):
+        """
+        Compute the gradient of an image using OpenCV Sobel filters.
 
+        Args:
+            image (Tensor): The image with shape (N, C, H, W).
+
+        Returns:
+            Tensor: The gradient magnitude with shape (N, C, H, W).
+        """
+        # Convert PyTorch tensor to NumPy array
+        image_np = image.detach().cpu().numpy()
+        N, C, H, W = image_np.shape
+        
+        gradient_magnitude = np.zeros((N, C, H, W), dtype=np.float32)
+        
+        for n in range(N):
+            for c in range(C):
+                img = image_np[n, c, :, :]
+                grad_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+                grad_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+                grad_mag = np.sqrt(grad_x ** 2 + grad_y ** 2)
+                gradient_magnitude[n, c, :, :] = grad_mag
+        
+        # Convert NumPy array back to PyTorch tensor
+        gradient_magnitude_tensor = torch.tensor(gradient_magnitude).to(image.device)
+        
+        return gradient_magnitude_tensor
+
+    def forward(self, input, target):
+        """
+        Compute the gradient loss between the input and target images.
+
+        Args:
+            input (Tensor): The predicted image with shape (N, C, H, W).
+            target (Tensor): The ground truth image with shape (N, C, H, W).
+
+        Returns:
+            Tensor: The Gradient Loss value.
+        """
+        # Compute gradients
+        gradient_input = self.compute_gradient(input)
+        gradient_target = self.compute_gradient(target)
+        
+        # Compute the loss
+        loss = F.mse_loss(gradient_input, gradient_target)
+        return loss
 
 
 
