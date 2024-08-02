d1a42fd4e5f97d412a23d77ce3380cedb88885c9
gusanagy <gustavo.pereira2146@gmail.com>
2024-08-02 07:45:01 -0300

    models parametros modificados

--- main.py
+++ main.py
@@ -5,29 +5,41 @@ from loss.perceptualLoss import *
 from models import Unet_model, VAE_model, Vit_model
 import torchinfo as t
 
+
+## carregar funcoes de perda
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 h = PerceptualLoss(id=4)
 
 print(h.name, h.id)
+## carregar modelos
 
-in_channels = 3  # Por exemplo, RGB
-out_channels = 3  # Saída de imagem RGB
-base_filters = 64
-num_layers = 4
-use_batch_norm = True
-model = Unet_model(in_channels, out_channels, base_filters, num_layers, use_batch_norm)
+
+# in_channels = 3  # Por exemplo, RGB
+# out_channels = 3  # Saída de imagem RGB
+# base_filters = 64
+# num_layers = 4
+# use_batch_norm = True
+# model = Unet_model(in_channels, out_channels, base_filters, num_layers, use_batch_norm)
 x = torch.randn(1, 3, 256, 256)
-print(model(x).shape,t.summary(model, input_size=(1, 3, 256, 256)))
+# print(model(x).shape,t.summary(model, input_size=(1, 3, 256, 256)))
+# Configuração de parâmetros
 latent_dim = 16
 hidden_dims = [32, 64, 128, 256]
 input_channels = 3
 output_channels = 3
-model = VAE_model(in_channels, out_channels, base_filters, num_layers, use_batch_norm)
-print(model(x).shape,t.summary(model, input_size=(1, 3, 256, 256)))
+model = VAE_model(input_channels=input_channels, hidden_dims=hidden_dims, latent_dim=latent_dim, output_channels=output_channels).to(device)
+print(x.shape,t.summary(model, input_size=(1, 3, 256, 256)))
 
-## carregar modelos
-## carregar funcoes de perda
+model = Vit_model(image_size=256, patch_size=16, num_channels=3, embed_dim=512, num_heads=8, mlp_dim=1024, num_layers=6, dropout=0.1)
+
+print(x.shape,t.summary(model, input_size=(1, 3, 256, 256)))
+
 ## carrregar datasets
+
 ## Fazer combinacao de modelos e funcoes de perda
+
 ## implementar ddp e torchrun para treinamento distribuido e acelerado
+
 ## Treinar modelo 
-## avaliar com as metricas
+
+## avaliar com as metricas // colocar metricas de validaçao
--- models/__init__.py
+++ models/__init__.py
@@ -7,7 +7,7 @@ from .vit import *
 
 def Unet_model(in_channels, out_channels, base_filters, num_layers, use_batch_norm):
     return UNet(in_channels, out_channels, base_filters, num_layers, use_batch_norm)
-def VAE_model(in_channels, out_channels, base_filters, num_layers, use_batch_norm):
-    return VAE(in_channels, out_channels, base_filters, num_layers, use_batch_norm)
-def Vit_model(in_channels, out_channels, base_filters, num_layers, use_batch_norm):
-    return ImageEnhancerTransformer(in_channels, out_channels, base_filters, num_layers, use_batch_norm)
+def VAE_model(input_channels, hidden_dims, latent_dim, output_channels):
+    return VAE(input_channels, hidden_dims, latent_dim, output_channels)
+def Vit_model(image_size=256, patch_size=16, num_channels=3, embed_dim=512, num_heads=8, mlp_dim=1024, num_layers=6, dropout=0.1):
+    return ImageEnhancerTransformer(image_size=image_size, patch_size=patch_size, num_channels=num_channels, embed_dim=embed_dim, num_heads=num_heads, mlp_dim=mlp_dim, num_layers=num_layers, dropout=dropout)
